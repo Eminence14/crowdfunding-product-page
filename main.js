091238ee6eab05a48089dc45cc4041b6ed5ee192
Victor <victoribidunmoye@gmail.com>
2022-09-21 12:46:01 +0100

    mobile modal default style completed

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const overlay = document.getElementById('overlay')
+const supportBtn = document.querySelector('#support-btn')
+const overlay2 = document.getElementById('overlay2')
+const modal = document.querySelector('.modal')
+const closeModalBtn = document.querySelector('p.close-modal')
 const hamburger = document.getElementById('hamburger')
 const close = document.getElementById('close-menu')
 const navbar = document.querySelector('.navbar')
+
+overlay2.addEventListener('click', closeModal)
+closeModalBtn.addEventListener('click', closeModal)
+
+supportBtn.addEventListener('click', () => {
+    modal.classList.add('active')
+    overlay2.classList.add('active')
+})
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.add('active')
@@ -18,4 +30,9 @@ function closeHamburger() {
     navbar.classList.remove('active')
     close.classList.remove('active')
     overlay.classList.remove('active')
+}
+
+function closeModal() {
+    modal.classList.remove('active')
+    overlay2.classList.remove('active')
 }
